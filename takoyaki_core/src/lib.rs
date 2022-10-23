cf8b177236121177645c79334f3d4eb7e1f45018
kyeboard <tokayaki01@gmail.com>
2022-10-23 12:02:40 +0530

    Remove tests

--- takoyaki_core/src/lib.rs
+++ takoyaki_core/src/lib.rs
@@ -20,44 +20,5 @@ mod tests {
     pub struct SamplePlugin {
 
     }
-
-    impl<'a> Plugin<'a , Sample> for SamplePlugin {
-        fn new() -> Self {
-            Self {
-
-            }
-        }
-
-        fn name(&self) -> &'a str {
-            "sample_plugin"
-        } 
-
-        fn ready(&self) -> ReadyState {
-            ReadyState::from_reqwest(reqwest::Client::new().get("https://jsonplaceholder.typicode.com/todos/1"))
-        }
-
-        fn execute(&self , _data: Sample) -> PrintableGrid {
-            PrintableGrid::new()
-        }
-    }
-
-
-    #[tokio::test]
-    #[should_panic]
-    async fn start_without_plug() {
-        let takoyaki = takoyaki::Takoyaki::<'static , Sample>::new();
-
-        takoyaki.start().await.unwrap(); // Would panic!
-    } 
-
-    #[tokio::test]
-    async fn start_with_plug() {
-        let mut takoyaki = takoyaki::Takoyaki::<Sample>::new();
-        let plugin = SamplePlugin::new();
-
-        takoyaki.plug(&plugin);
-
-        takoyaki.start().await.unwrap(); // Would not panic =)
-    }
 }
 

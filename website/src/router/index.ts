1aa55fff974cb2bf91512fa282af2004db451746
kyeboard <tokayaki01@gmail.com>
2022-10-24 20:02:20 +0530

    updates

--- website/src/router/index.ts
+++ website/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Marketplace from "../views/Marketplace.vue"
 import HomeView from "../views/HomeView.vue";
+import Install from "../views/Install.vue";
+import Me from "../views/Me.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,18 @@ const router = createRouter({
             path: "/",
             name: "home",
             component: HomeView,
+        },
+        {
+            path: "/install",
+            component: Install,
+        },
+        {
+            path: "/me",
+            component: Me,
+        },
+        {
+            path: "/marketplace",
+            component: Marketplace,
         },
     ],
 });

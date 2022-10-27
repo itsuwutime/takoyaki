0dcb2fe43e137579f153df2892c76dbbf63ff92a
kyeboard <tokayaki01@gmail.com>
2022-10-27 18:07:01 +0530

    Updates

--- website/src/views/Marketplace.vue
+++ website/src/views/Marketplace.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="w-screen h-screen bg-base"></div>
-    <button
-        class="p-5 flex items-center justify-center p-2 border-none rounded-xl bg-blue/100 fixed bottom-6 right-6"
-    >
-        <vue-feather type="plus" />
-    </button>
+    <div class="w-screen h-screen bg-base pt-32 px-10">
+        <div class="flex">
+            <input class="w-full text-text focus:outline-none p-4 bg-surface0/40 rounded-lg placeholder-overlay1 px-6" placeholder="Search for the plugins..." />
+            <a href="/marketplace/new"><button class="w-72 ml-4 p-4 bg-blue rounded-lg placeholder-overlay1 px-6">Create a new plugin</button></a>
+        </div>
+    </div>
+    <!-- <button -->
+    <!--     class="p-5 flex items-center justify-center p-2 border-none rounded-xl bg-blue/100 fixed bottom-6 right-6" -->
+    <!-- > -->
+    <!--     <vue-feather type="plus" /> -->
+    <!-- </button> -->
 </template>

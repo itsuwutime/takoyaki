5ba5d78725578ce33a5b42a223e00f11519f255f
kyeboard <tokayaki01@gmail.com>
2022-10-30 07:44:18 +0530

    feat(website): Add marketplace to show all the plugins

--- website/src/views/Marketplace.vue
+++ website/src/views/Marketplace.vue
@@ -1,13 +1,35 @@
 <template>
-    <div class="w-screen h-screen bg-base pt-32 px-44">
-        <div class="flex">
-            <input class="w-full text-text focus:outline-none p-4 bg-surface0/40 rounded-lg placeholder-overlay1 px-6" placeholder="Search for the plugins..." />
-            <a href="/marketplace/new"><button class="w-72 ml-4 p-4 bg-blue rounded-lg placeholder-overlay1 px-6">Create a new plugin</button></a>
+    <div class="w-screen h-screen bg-base text-text pt-44 px-44">
+        <div class="text-3xl font-bold">Plugins</div>
+        <div class="text-overlay1 font-semibold mt-2">Awesome plugins built by awesome developers</div>
+        <div class="mt-10 flex gap-x-12 flex-wrap gap-y-6">
+            <a class="plugin bg-surface0/20 p-6 rounded-lg w-96" v-for="plugin in plugins.plugins" :key="plugin.name" :href="'/plugin/' + plugin.name.toLowerCase()">
+                <div class="flex flex-col h-full">
+                    <img :src="plugin.image" class="w-[70px] mr-4 float-left" />
+                    <div class="info mt-5 mb-4">
+                        <div class="title text-xl font-bold">{{plugin.name}}</div>
+                        <div class="title text-overlay2 font-semibold mt-2 leading-7">{{plugin.description}}</div>
+                    </div>
+                    <div class="mt-auto flex items-center font-semibold text-blue">
+                        <vue-feather type="link" size="19" class="mr-2" /> <a :href="plugin.home_page">{{plugin.home_page}}</a>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
-    <!-- <button -->
-    <!--     class="p-5 flex items-center justify-center p-2 border-none rounded-xl bg-blue/100 fixed bottom-6 right-6" -->
-    <!-- > -->
-    <!--     <vue-feather type="plus" /> -->
-    <!-- </button> -->
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const plugins = ref<Array<any>>([]);
+
+(async () => {
+    const res = await fetch("https://raw.githubusercontent.com/kyeboard/takoyaki/main/plugins/plugins.json");
+
+    plugins.value = await res.json();
+
+    console.log(plugins.value)
+})()
+</script>
+

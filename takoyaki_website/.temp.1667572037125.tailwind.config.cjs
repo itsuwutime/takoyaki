3b5fb39037c6ba780fd2dc3ab024ca90c5bcfc3b
kyeboard <tokayaki01@gmail.com>
2022-11-04 20:56:48 +0530

    feat: Add navbar

--- takoyaki_website/.temp.1667572037125.tailwind.config.cjs
+++ takoyaki_website/.temp.1667572037125.tailwind.config.cjs
@@ -3,13 +3,11 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
         extend: {
-
+            colors: {
+                base: "#1A1F29",
+                surface: 
+            }
         },
 	},
-    plugins: [
-        require('@catppuccin/tailwindcss')({
-            prefix: '',
-            defaultFlavour: 'mocha'
-        }),
-    ],
+	plugins: [],
 }

0b67730cdc911725a5c1ea03f0135ce8fe021356
kyeboard <tokayaki01@gmail.com>
2022-11-05 12:07:57 +0530

    feat(website): Import code from Astro home page

---
+++ takoyaki_website/src/components/NavBar.vue
@@ -0,0 +1,76 @@
+<template>
+    <div
+        class="navbar px-16 w-full fixed items-center justify-center text-text font-semibold top-0 left-0 py-6 flex border-b border-b-surface0/60"
+    >
+        <div class="left flex items-center justify-center">
+            <img src="/logo.png" class="h-10" />
+            <div class="ml-6 text-text font-bold text-lg">takoyaki</div>
+        </div>
+        <div class="mx-auto text-overlay2 links flex gap-x-14">
+            <a href="/">Home</a>
+            <a href="/installation">Installation</a>
+            <a href="/">Documentation</a>
+            <a href="/">Marketplace</a>
+        </div>
+        <div class="current-user flex items-center justify-center hidden">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#cdd6f4"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            >
+                <line x1="4" y1="21" x2="4" y2="14"></line>
+                <line x1="4" y1="10" x2="4" y2="3"></line>
+                <line x1="12" y1="21" x2="12" y2="12"></line>
+                <line x1="12" y1="8" x2="12" y2="3"></line>
+                <line x1="20" y1="21" x2="20" y2="16"></line>
+                <line x1="20" y1="12" x2="20" y2="3"></line>
+                <line x1="1" y1="14" x2="7" y2="14"></line>
+                <line x1="9" y1="8" x2="15" y2="8"></line>
+                <line x1="17" y1="16" x2="23" y2="16"></line>
+            </svg>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                class="ml-10"
+                height="18"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#cdd6f4"
+                stroke-width="2.4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            >
+                <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+                <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+            </svg>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+                class="ml-10"
+                fill="none"
+                stroke="#cdd6f4"
+                stroke-width="2.4"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            >
+                <circle cx="11" cy="11" r="8"></circle>
+                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+            <img
+                src="https://avatars.githubusercontent.com/u/115910279?v=4"
+                class="h-10 rounded-full ml-16"
+            />
+        </div>
+        <button class="login bg-surface0/40 text-text p-3 px-10 rounded-lg">
+            Login
+        </button>
+    </div>
+</template>

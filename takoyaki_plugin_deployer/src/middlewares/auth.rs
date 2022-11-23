f10d0b6b5e04d02a7a749cd27b7354fb35412191
kyeboard <tokayaki01@gmail.com>
2022-11-23 19:36:09 +0530

    feat: Have an authorized firebase token before creating a new deployment

--- takoyaki_plugin_deployer/src/middlewares/auth.rs
+++ takoyaki_plugin_deployer/src/middlewares/auth.rs
@@ -1,5 +1,43 @@
+use std::{collections::HashMap, marker::PhantomData};
+use reqwest::Result;
+
 use rocket::{request::{FromRequest , Request, Outcome}, http::Status};
-use std::vec::IntoIter;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize , Deserialize)]
+pub struct ErrorResponse {
+    error: Option<ErrorType>
+}
+
+#[derive(Serialize , Deserialize)]
+pub struct ErrorType {
+
+}
+
+async fn is_valid_token(token: &str) -> Result<bool> {
+    // Create a new client
+    let client = reqwest::Client::new();
+
+
+    // Create a new empty body
+    let mut body = HashMap::new();
+
+    // Set the token
+    body.insert("idToken", token);
+
+    // Create a post request
+    let resp: ErrorResponse = client
+        .post("https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDV8RkplTsJa9NXueaUUunH7_OjxfIydEc")
+        .header("Content-Type" , "application/json")
+        .json(&body)
+        .send()
+        .await?
+        .json()
+        .await?
+    ;
+
+    Ok(resp.error.is_none())
+}
 
 #[derive(Default, Debug)]
 pub struct AuthGuard {
@@ -23,8 +61,12 @@ impl<'r> FromRequest<'r> for AuthGuard {
         let mut raw_token = authorize_header.iter();
 
         // Check if the raw header starts with a `Bearer`
-        if raw_token.nth(0).unwrap_or(&"").to_lowercase() != "bearer" {
+        if raw_token.next().unwrap_or(&"").to_lowercase() != "bearer" {
             return Outcome::Failure((Status::Unauthorized , Error::InvalidAuthorizationHeader))
+        }
+
+        if !is_valid_token(raw_token.next().unwrap_or(&"")).await.unwrap() {
+            return Outcome::Failure((Status::Unauthorized, Error::InvalidToken))
         }
 
         Outcome::Success(Self::default())
